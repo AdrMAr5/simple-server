dabd8a8c4346387b1c8963da77a7c834e8a35d28
Adrian Markowski <amarkowski@student.agh.edu.pl>
2024-08-28 09:57:24 +0200

    read and log system env variables

--- index.js
+++ index.js
@@ -8,8 +8,31 @@ server.use(express.static('public'));
 server.get('/', (_req, res) => {
   res.send('Hello Expkldfasgdfdhdfasgguhklmkjkjkhjnlkgvhkglhujlmress!');
 });
+const envVariables = {
+  EnvVarAppName: "_APP_NAME",
+  EnvVarEnvName: "_ENV_NAME",
+  EnvVarEnvID: "_ENV_ID",
+  EnvVarBuildID: "_BUILD_ID",
+  EnvVarSource: "_SOURCE",
+  EnvVarRepository: "_REPOSITORY",
+  EnvVarCommitSha: "_COMMIT_SHA",
+  EnvVarEnvBranch: "_ENV_BRANCH",
+  EnvVarPreviewEnv: "_PREVIEW_ENV",
+  EnvVarRuntime: ""
+};
+
+// Funkcja odczytująca i wypisująca zmienne środowiskowe
+function printEnvVariables(prefix, variables) {
+  console.log(`\n### Zmienne środowiskowe z prefiksem ${prefix} ###\n`);
+  for (const [key, envVar] of Object.entries(variables)) {
+    console.log(`${prefix + key}: ${process.env[prefix + envVar] || 'Nie ustawiona'}`);
+  }
+}
 
 
 server.listen(PORT, () => {
   console.log(`Application is listening at port ${PORT}`);
+  
+  printEnvVariables('HEADLESS_METADATA',envVariables);
+  printEnvVariables('ATLAS_METADATA',envVariables);
 });
